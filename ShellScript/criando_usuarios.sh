687e6b1ff6144fbb37d442c0d05320517914cde8
Nicole Silva - Padrao <nicolesilva8144@gmail.com>
2023-03-06 11:25:21 -0300

    Scripts

--- ShellScript/criando_usuarios.sh
+++ ShellScript/criando_usuarios.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-echo "Criando usuário convidado..."
+#Script criado com a finalidade de praticar a estrutura de usuários,diretórios e permissões.
+#06/03/2023
 
-#Criando usuário e senha 
-useradd guest -c "Usuário Convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123) 
+echo "Criando diretórios ..."
 
-#Força o usuário a criar uma nova senha
-passwd guest -e
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
 
-echo "Usuário criado!"
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários e adicionando aos grupos..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiano -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Procedimentos finalizados"
